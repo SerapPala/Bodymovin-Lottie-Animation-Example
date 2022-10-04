c64feaf6d547abf5d6f3ba980ef2cec2face8c46
Serap Pala <serap68040@gmail.com>
2022-10-04 13:48:50 +0300

    tamamlandi

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+    /* Shapes */
+    var svgContainer = document.getElementById('svgContainer');
+    var animItem = bodymovin.loadAnimation({
+      wrapper: svgContainer,
+      animType: 'svg',
+      loop: true,
+      path: '70608-letter-s.json'
+    });
